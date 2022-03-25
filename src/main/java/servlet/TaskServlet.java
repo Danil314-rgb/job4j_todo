0cffd85b43548771846eb1c0f7771f3310f46182
Danil <totem7h@gmail.com>
2022-03-25 14:47:16 +0400

    2. Создать TODO list [#3786]

--- src/main/java/servlet/TaskServlet.java
+++ src/main/java/servlet/TaskServlet.java
@@ -14,15 +14,12 @@ public class TaskServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //req.setAttribute("allTasks", DbStore.instOf().allTasks());
-        //req.setAttribute("taskDoneTrue", DbStore);
         req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        //DbStore.instOf().addTask(new Task(req.getParameter("description")));
         DbStore.instOf().addTask(new Task(req.getParameter("description"), LocalDateTime.now(), false));
         resp.sendRedirect(req.getContextPath() + "/index.jsp");
     }
